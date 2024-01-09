2dfd0f4fffc8b86c5f8961818165ab121759a2e9
Abhijith Subash <abhijithsubash07@gmail.com>
2024-01-09 16:31:45 +0530

    feat: accessed token from url

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import Login from './pages/Login';
+import { useEffect, useState } from 'react'
+import { getTokenFromUrl } from './api/Spotify'
+
 
 function App() {
+  const [token, setToken] = useState(null)
+
+  useEffect(()=>{
+        const hash = getTokenFromUrl()
+        window.location.hash = ""
+        const _token = hash.access_token
+        if(_token){setToken(_token)}
+    }, [])
+
   return (
     <div className="App">
-      <Login />
+      {token?<h1>Logged in</h1>:<Login />}
     </div>
   );
 }
